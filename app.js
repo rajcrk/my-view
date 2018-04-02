465d8535d2f02fbd8f18fafd153740f56eab3505
Raj Kumar <rajkumar.cind@gmail.com>
2018-04-02 18:57:30 +0530

    work-2

--- app.js
+++ app.js
@@ -215,8 +215,43 @@ app.get("/staff-mark-view/:batch/:subject/:sem", function(req, res){
 //===============================
 //CHANGING THE GRADE OF A STUDENT
 //===============================
-app.post("/staff-mark-view/:batch/:subject/:sem", function(req, res){
+app.post("/staff-mark-view/student-grade-change", function(req, res){
+    var myQuery = { 
+        _id: req.body.changeId,
+        SEM1: Object
 
+     };
+    var changeSem_tmp = req.body.changeSem;
+    var changeGrade_tmp = req.body.changeGrade;
+    var name_tmp = req.body.changeSub; 
+    var objToMe = new Object;
+    objToMe[changeSem_tmp];
+    var objToMe2 = new Object;
+    objToMe2[name_tmp] = changeGrade_tmp;
+    objToMe[changeSem_tmp] = objToMe2;
+    console.log(objToMe);
+    console.log(objToMe2);
+
+    //var updateVal = {};
+    console.log("new Grade");
+    console.log(changeGrade_tmp);
+    console.log("Semester to change");
+    console.log(changeSem_tmp);
+    
+    changeSem_tmp[name_tmp] = changeGrade_tmp;
+    console.log("Subject to be changed:");
+    console.log(name_tmp);
+    console.log("the obj to be passed");
+    console.log(changeSem_tmp.name_tmp);
+    var newValues = { $set: objToMe2 }
+    Student.updateOne(myQuery, newValues, function(err, res){
+        if(err) console.log(err);
+        console.log("SEM Grade Maupulated Ha Ha Ha !. That`s evil laugh BTW ");
+        console.log(res);
+        
+    });
+    res.send("hi");
+    //res.redirect("/my-user-profile/"+req.user._id);
 });
 
 
@@ -280,13 +315,13 @@ app.post("/user-roll-get", function(req, res){
 //========================//
 // Development Port Number//
 //========================//
-// app.listen(3300, function(){
-//     console.log("The Server Has Started at 3300");
-// });
+app.listen(3300, function(){
+    console.log("The Server Has Started at 3300");
+});
 
 //========================//
 // Production Port Number//
 //========================//
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The Server Has Started");
-});
+// app.listen(process.env.PORT, process.env.IP, function(){
+//     console.log("The Server Has Started");
+// });
